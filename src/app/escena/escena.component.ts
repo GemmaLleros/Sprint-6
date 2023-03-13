f7966bf3c76f2d55df342a0d77c63cb28f4ba10c
GemmaLleros <gemma.caballero.serra@gmail.com>
2023-03-13 20:11:43 +0100

    Exercise-2

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Sentence } from '../home/interfaces/sentence';
 
 @Component({
   selector: 'app-escena',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./escena.component.css']
 })
 export class EscenaComponent {
-
+  @Input() sentences!: Sentence[];
 }
